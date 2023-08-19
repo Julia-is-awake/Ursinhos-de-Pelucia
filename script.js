af2fea394254fc5e04b5d7c76534d358f9904b29
Rexzinho <viniciussorgetzalves@gmail.com>
2023-08-19 13:52:02 -0300

    add carrossel
    Co-authored-by: Julia <Julia-is-awake@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,48 @@
+function trocar (f) {
+    document.getElementById("imagem-do-produto").src=f;
+}
+
+const imagens = ['https://c1.wallpaperflare.com/preview/570/887/752/emotions-toys-teddy-bear-soft-toy.jpg', 'https://c1.wallpaperflare.com/preview/535/980/944/stuffed-animals-toys-teddy-bear.jpg', 'https://t3.ftcdn.net/jpg/03/29/09/42/360_F_329094223_QHjakaco7v8zxDRR3EDqpBvJVJHDCPYT.jpg']
+
+var indexImagem = 0;
+
+function slider (number) {
+    console.log("Oi VIni!")
+    console.log("O Tamanho da lista é " + imagens.length)
+    indexImagem += number
+    console.log("O index é " + indexImagem)
+    if(indexImagem < 0)
+    {
+        console.log("Entro no if :D ")
+        indexImagem = imagens.length -1
+        console.log("O index é " + indexImagem)
+    }
+    if(indexImagem >= imagens.length)
+    {
+        console.log("Entro no else")
+        indexImagem = 0
+        console.log("O index é " + indexImagem)
+
+    }
+    document.getElementsByClassName('Carrocel')[0].style.backgroundImage = `url('${imagens[indexImagem]}')`
+    console.log(indexImagem)
+}
+
+/* //setar contador inicial
+let count = 0;
+
+//selecionando os botoes e valores
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count--;
+        }
+        value.textContent = count;
+    });
+});
+ */
+/* function menuSw */
